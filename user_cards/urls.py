3934ac2726a56f40f59634597791bb825257a6f3
Oxy <417ksena@gmail.com>
2022-03-03 22:23:47 +0200

    add card form

--- user_cards/urls.py
+++ user_cards/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('add_card', views.add_card, name='add_card'),
     path('<int:card>', views.add_sum, name='add_sum'),
     path('', views.index, name='index'),
 ]
--- user_cards/views.py
+++ user_cards/views.py
@@ -1,4 +1,4 @@
-from .forms import CardSumForm
+from .forms import CardSumForm, CardForm
 from django.shortcuts import render, redirect
 from .models import Card, CardSum
 from django.db.models import Sum
@@ -7,8 +7,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     all_cards = Card.objects.all().annotate(card_total_sum=Sum('cardsum__sum'))
+    card_form = CardForm()
     context = {
         'all_cards': all_cards,
+        'card_form': card_form
     }
     return render(request, 'index.html', context)
 
@@ -21,4 +23,13 @@ def add_sum(request):
             card_sum_form.save()
             return redirect("/user_cards")
 
+    return redirect("/user_cards")
+
+
+@csrf_exempt
+def add_card(request):
+    if request.POST:
+        card_form = CardForm(request.POST)
+        if card_form.is_valid:
+            card_form.save()
     return redirect("/user_cards")
